4aff881aa2d6a79cca6c3698be3cbaea03710c7e
Ben Youngblood <bx.youngblood@gmail.com>
2012-08-02 11:25:57 -0700

    initial commit

---
+++ config/module.config.php
@@ -0,0 +1,49 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'speckaddress' => 'SpeckAddress\Controller\AddressController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'speckaddress' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    // Change this to something specific to your module
+                    'route'    => '/address',
+                    'defaults' => array(
+                        // Change this value to reflect the namespace in which
+                        // the controllers for your module are found
+                        'controller'    => 'speckaddress',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    // This route is a sane default when developing a module;
+                    // as you solidify the routes for your module, however,
+                    // you may want to remove it and replace it with more
+                    // specific routes.
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'SpeckAddress' => __DIR__ . '/../view',
+        ),
+    ),
+);
---
+++ src/SpeckAddress/Controller/AddressController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SpeckAddress\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class AddressController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+}
